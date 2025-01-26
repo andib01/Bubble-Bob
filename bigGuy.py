379071582c6b71ec9c3563731edb871a343595ca
Rana Kelani <rana.kelani@gmail.com>
2025-01-26 13:44:36 +0100

    Add bubble and big guy graphics

--- bigGuy.py
+++ bigGuy.py
@@ -10,7 +10,8 @@ class BigGuy(pygame.sprite.Sprite):
             "idle": pygame.image.load("assets/big_guy/idle.png").convert_alpha(),
             "eat1": pygame.image.load("assets/big_guy/eat_1.png").convert_alpha(),
             "eat2": pygame.image.load("assets/big_guy/eat_2.png").convert_alpha(),
-            "eat3": pygame.image.load("assets/big_guy/eat_3.png").convert_alpha()
+            "eat3": pygame.image.load("assets/big_guy/eat_3.png").convert_alpha(),
+            "eat4": pygame.image.load("assets/big_guy/eat_4.png").convert_alpha(),
         }
         graphics_helper.scale_surfaces(self.images, 120)
 
@@ -19,16 +20,30 @@ class BigGuy(pygame.sprite.Sprite):
         self.current_state = "idle"
         self.image = self.images[self.current_state]
         self.rect = self.image.get_rect(bottomleft=(5, SCREEN_HEIGHT))
+
+        self.angle = 0
+        self.update()
+
+    def update(self):
+        self.angle += 1
+        if self.angle >= 360:
+            self.angle = 0
+        self.image = pygame.transform.rotate(self.images[self.current_state], self.angle)
+        bottomleft = self.rect.bottomleft
+        self.rect = self.image.get_rect()
+        self.rect.bottomleft = bottomleft
       
 
     def eat(self, bubble_count):
         if bubble_count == 4:
             self.current_state = "eat1"
-        elif bubble_count == 8:
+        elif bubble_count == 6:
             self.current_state = "eat2"
-        elif bubble_count == 12:
+        elif bubble_count == 10:
             self.current_state = "eat3"
-            # TODO: Here the big guy should be ready to fight the evil guy
+        elif bubble_count == 12:
+            self.current_state = "eat4"    
+            # TODO: Here the main guy should be ready to fight the evil guy
 
 
         # TODO: Add sound effect for eating: e.g. burbing sound     
--- bubble.py
+++ bubble.py
@@ -5,12 +5,24 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 class Bubble(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.Surface((20, 20))
-        self.image.fill((0, 255, 0))
+
+        self.original_image = pygame.image.load("assets/bubble.png").convert_alpha()
+        self.original_image = pygame.transform.smoothscale(self.original_image, (50, 50))
+
+        self.image = self.original_image
         self.rect = self.image.get_rect(center=(x, y))
         self.speed = random.randint(3, 6)
+        self.angle = 0
+
+
+        self.image.set_alpha(180)
 
     def update(self):
         self.rect.y += self.speed
+        
+        self.angle += 1
+        old_center = self.rect.center
+        self.image = pygame.transform.rotate(self.original_image, self.angle)
+        self.rect = self.image.get_rect(center=old_center)
         if self.rect.top > SCREEN_HEIGHT:
             self.kill()
