f89be4fe89acf2663897ffc5d9f40ca13dbee8c7
Rana Kelani <rana.kelani@gmail.com>
2025-01-26 01:23:05 +0100

    Add blade graphic effects

--- blade.py
+++ blade.py
@@ -4,12 +4,21 @@ from constants import SCREEN_WIDTH
 class Blade(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.Surface((10, 30))
-        self.image.fill((255, 0, 0))
+        self.original_image = pygame.image.load("assets/blade.png").convert_alpha()
+        self.original_image = pygame.transform.smoothscale(self.original_image, (20, 20))
+
+        self.image = self.original_image
         self.rect = self.image.get_rect(center=(x, y))
+
         self.speed = 7
+        self.angle = 0 
 
     def update(self):
         self.rect.x -= self.speed
+        self.angle += 5
+        old_center = self.rect.center
+        self.image = pygame.transform.rotate(self.original_image, self.angle)
+        self.rect = self.image.get_rect(center=old_center)
+
         if self.rect.right < 0:
             self.kill()
